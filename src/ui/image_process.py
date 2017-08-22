10080b6e87e850712453ec24f8fb9f94baa14c9d
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-08-22 19:24:49 +0200

    loading image into matplotlib/gtk canvas

--- src/ui/image_process.py
+++ src/ui/image_process.py
@@ -5,11 +5,25 @@ import copy
 import os
 
 from gi.repository import Gtk, Gdk
-from PIL import Image
+
+from scipy import misc
+
+import matplotlib.pyplot as plt
+
+from PIL.Image import open as pilload
+
+from matplotlib.image import imread as pltimread
+
+#from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as GtkFigureCanvas
+from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as GtkFigureCanvas
+from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as NavigationToolbar
+
+#from lib.convimage import ConvImage as CIm
 
 UI_FILE = __name__ + ".ui"
 
-default_image = 'Images/PSPyoda.gif'
+#default_image_path = 'Images/PSPyoda.gif'
+default_image_path = '/home/nemosyne/work/PolSARpro/doc_n_data_set/SAN_FRANCISCO_ALOS/T3/PauliRGB.bmp'
 
 
 class GUI:
@@ -30,37 +44,38 @@ class GUI:
         self.init_image()
 
     def init_image(self):
-        # will use a pil thumbnail for the GtkImage
 
-        # Setting pillow image
+        imBox = self.builder.get_object('dialog-vbox1')
         if self.image_file_path is None:
             self.image_file_path = \
-                self.config['localDir'] + '/' + default_image
+                default_image_path
+                #self.config['localDir'] + '/' + default_image_path
 
-        self.image =  Image.open(self.image_file_path)
+        self.image = pilload(self.image_file_path)
         
         max_width = Gdk.Screen.width() / 2 - 100
         max_height = Gdk.Screen.height() / 2 - 100
 
+        #self.window.set_size_request(max_width, max_height)
+
         # Setting Gtk image
-
-        self.GtkImage = self.builder.get_object('main_image')
-        self.GtkImage_width, self.GtkImage_height = self.image.size
-    
-        self.thumb_path = self.config['tempDir'] + \
-            os.path.basename(self.image_file_path)
-
-        self.thumb = self.image.copy()
-
-        if self.GtkImage_width > max_width and \
-            self.GtkImage_height > max_height:
-
-            # resize to max allowed size
-            self.thumb.thumbnail(max_width, max_height)
-    
-        self.thumb.save(self.thumb_path)
-
-        self.GtkImage.set_from_file(self.thumb_path)
         
+        self.GtkSw = self.builder.get_object('scrolledwindow_image')
+        width, height = self.image.size
+    
+        fig, ax = plt.subplots()
+        ax.imshow(self.image, origin='upper')
+
+        canvas = GtkFigureCanvas(fig)
+        self.GtkSw.add_with_viewport(canvas)
+
+        if width > max_width or height > max_height:
+            # resize to max allowed size
+            self.window.resize(max_width, max_height)
+        else:
+            self.window.resize(width, height)
+            
+        self.window.show_all()
+
     def on_rect_selection_button_clicked(self, widget, *args):
         pass
